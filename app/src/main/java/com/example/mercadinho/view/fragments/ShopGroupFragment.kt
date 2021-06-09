2321e2d18752bca26faa2c95d0f1ec377d927340
Kevin-Mattos <kevinmmattos@gmail.com>
2021-06-09 14:33:12 -0300

    add navigation component

--- app/src/main/java/com/example/mercadinho/view/fragments/ShopGroupFragment.kt
+++ app/src/main/java/com/example/mercadinho/view/fragments/ShopGroupFragment.kt
@@ -1,30 +1,26 @@
 package com.example.mercadinho.view.fragments
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.mercadinho.MainActivity
 import com.example.mercadinho.databinding.CreateCustomDialogBinding
 import com.example.mercadinho.databinding.MainFragmentBinding
 import com.example.mercadinho.repository.entities.ShopGroup
-import com.example.mercadinho.repository.entities.ShopItem
 import com.example.mercadinho.view.adapter.ShopGroupAdapter
 import com.example.mercadinho.viewmodels.ShopGroupFragmentViewModel
 import com.example.mercadinho.viewmodels.ShopGroupListFragmentIntent
 import com.example.mercadinho.viewmodels.ShopGroupListFragmentState
 
 class ShopGroupFragment : Fragment(), ShopGroupAdapter.GroupAction, MainActivity.FabAction {
-
-    companion object {
-        fun newInstance() = ShopGroupFragment()
-    }
 
     private val mBinding by lazy {
         MainFragmentBinding.inflate(layoutInflater)
@@ -38,22 +34,29 @@ class ShopGroupFragment : Fragment(), ShopGroupAdapter.GroupAction, MainActivity
         activity as MainActivity
     }
 
-    private val mViewModel: ShopGroupFragmentViewModel by lazy {
-        createViewModel()
+    private val mViewModel: ShopGroupFragmentViewModel by viewModels()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mMainActivity.fabCallback = this::fabClicked
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        observeGroups()
-        mViewModel.handle(ShopGroupListFragmentIntent.GetAllGroups)
-        setupAdapter()
         return mBinding.root
     }
 
-    override fun onClick(groupId: Long) {
-        mMainActivity.replaceThis(groupId)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeGroups()
+        mViewModel.handle(ShopGroupListFragmentIntent.GetAllGroups)
+        setupAdapter()
+    }
+
+    override fun onClick(groupId: Long) = findNavController().run {
+        navigate(ShopGroupFragmentDirections.actionShopGroupFragmentToShopItemFragment(groupId))
     }
 
     override fun onLongClick(group: ShopGroup) {
@@ -85,12 +88,6 @@ class ShopGroupFragment : Fragment(), ShopGroupAdapter.GroupAction, MainActivity
 
     private fun setupAdapter() {
         mBinding.myRecyclerView.adapter = mAdapter
-    }
-
-    private fun createViewModel(): ShopGroupFragmentViewModel {
-        return ViewModelProvider.AndroidViewModelFactory
-            .getInstance(requireActivity().application)
-            .create(ShopGroupFragmentViewModel::class.java)
     }
 
     private fun observeGroups() {
